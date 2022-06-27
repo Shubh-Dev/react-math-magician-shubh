5034e3442cf12bf46d80ce38572e40c606366258
Shubh <shubh.scb@gmail.com>
2022-06-27 22:58:14 +0530

    CLASS COMPONENT USED

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <>
-      <Calculator />
-    </>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Calculator />
+      </>
+    );
+  }
 }
 
 export default App;
