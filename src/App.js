3a9e91147d61f69641c758fb25954ced98fd430f
Shubh <shubh.scb@gmail.com>
2022-06-28 19:20:46 +0530

    HOOK ADD CLASS REMOVE

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Calculator />
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Calculator />
+  </>
+);
 
 export default App;
