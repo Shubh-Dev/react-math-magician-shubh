e6a861185a55a3d963bff202b8c6d14e1713d2be
Shubh <shubh.scb@gmail.com>
2022-06-30 16:15:17 +0530

    ADD ROUTER PROGRESS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as router, Routes, Route, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 
 const App = () => (
   <>
     <Calculator />
+    <Home />
   </>
 );
 
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+  <h1>Router Test</h1>
+);
+
+export default Home;
