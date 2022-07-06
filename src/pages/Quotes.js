be978cf56f63878b32e45381efb56c169f53209f
Shubh <shubh.scb@gmail.com>
2022-07-06 12:04:17 +0530

    QUOTES ADD

---
+++ src/pages/Quotes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Quote.css';
+
+const Quotes = () => (
+  <div className="quotesSection">
+    <p>
+      Mathematics is not about numbers, equation, computations or algorithms:
+      it is about understanding.- Willium Paul Thurston.
+    </p>
+  </div>
+);
+
+export default Quotes;
