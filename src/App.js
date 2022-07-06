6b1a7d4c8196c24fe8485b71892493279fb6d641
Shubh <shubh.scb@gmail.com>
2022-07-06 12:00:55 +0530

    HEADER ADD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as router, Routes, Route, Link,
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as router, Routes, Route, Link,
+// } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
 
