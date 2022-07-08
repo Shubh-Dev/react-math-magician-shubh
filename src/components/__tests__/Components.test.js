ef73cf17f681ba46d4de861f05e749448972dd50
Abdulhamid <adioabdulhamid@yahoo.com>
2022-07-08 12:10:49 +0100

    Fix eslint errors

--- src/components/__tests__/Components.test.js
+++ src/components/__tests__/Components.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Home from '../../pages/Home'
+import Home from '../../pages/Home';
 import Quotes from '../../pages/Quotes';
 import Calculator from '../Calculator';
 
 describe('Calculater component test', () => {
-    test('builds the snapshot of the Calculator component', () => {
-        const tree = renderer.create(<Calculator />);
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
+  test('builds the snapshot of the Calculator component', () => {
+    const tree = renderer.create(<Calculator />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 
-    test('builds the snapshot of the Home component', () => {
-        const tree = renderer.create(<Home />);
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
+  test('builds the snapshot of the Home component', () => {
+    const tree = renderer.create(<Home />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 
-    test('builds the snapshot of the Quotes component', () => {
-        const tree = renderer.create(<Quotes />);
-        expect(tree.toJSON()).toMatchSnapshot();
-    })
+  test('builds the snapshot of the Quotes component', () => {
+    const tree = renderer.create(<Quotes />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
