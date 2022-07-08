612272995f4520369502bee7eb41d9af1ae5741e
Shubh <shubh.scb@gmail.com>
2022-07-08 14:02:36 +0530

    UI UPDATE

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => (
   <header className="header">
     <span href="/">
-      <Link style={{ textDecoration: 'none', color: '#fff', fontSize: '70px' }} to="/">Math Magicians Shubh-Krishang Das</Link>
+      <Link style={{ textDecoration: 'none', color: '#fff', fontSize: '70px' }} to="/">Math Magicians</Link>
     </span>
     <div className="navigation">
       <Link className="nav" to="/">Home</Link>
