19fc4799fed23899e03f86f70bb67473957ae6de
Shubh <shubh.scb@gmail.com>
2022-07-08 18:13:55 +0530

    THREE TESTD ADD TO CALCULATE TEST

--- src/logic/Calculate.test.js
+++ src/logic/Calculate.test.js
@@ -24,4 +24,30 @@ describe('state object operation test', () => {
 
     expect(result.total).toEqual('10');
   });
+
+  test('test object total', () => {
+    const stateObj = { total: null, next: '10', operation: null };
+    const button = '-';
+    const result = calculate(stateObj, button);
+
+    expect(result.total).not.toBe('7');
+  });
+
+  test('test division', () => {
+    const stateObj = { total: 30, next: '5', operation: null };
+    const button = '÷';
+    const result = calculate(stateObj, button);
+
+    expect(result.total).not.toBe('6')
+  })
+
+  test('clear button test', () => {
+    const stateObj = { total: null, next: null, operation: null };
+    const button = 'AC';
+    const result = calculate(stateObj, button);
+
+    expect(result.total).toBe(null);
+  })
+
+
 });
